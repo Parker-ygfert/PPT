2f427f58676489864bbfc55824fc4fbca010c1aa
Parker <ygfert@gmail.com>
2020-07-24 16:55:25 +0800

    Board Read and Create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,10 +5,34 @@ class BoardsController < ApplicationController
   end
   
   def new
+    @board = Board.new
   end
 
   def create
-    render html: params
+    # @board = Board.new(params[:board])
+    #* 參數未經過清洗會被擋住
+
+    @board = Board.new(board_params)
+
+    if @board.save
+      # flash[:notice] = "新增成功"
+      redirect_to boards_path, notice: "新增成功"
+    else
+      render :new
+    end
+
+    # Board.creat(title: params[:tilte], intro: params[:intro])
+    # redirect_to '/'
+    #* 手刻表單要自己抓出參數
+    #* form helper 自動生成 name="board[title]" 會整合成一包
+    #* params => {"board" => {"title" => "aaa", "intro" => "bbb"}}
+    #* 這樣抓參數可以直接用 params[:board]
+  end
+
+  private
+  def board_params
+    #* Strong Parameters
+    params.require(:board).permit(:title, :intro)
   end
   
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+
+  #* 為何表單驗證是在 Model 這邊做?
+  validates :title, presence: true, length: { minimum: 2 }
+
 end
