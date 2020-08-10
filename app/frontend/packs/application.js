418ba5a452bec0b7febd4f88b63c469a7160c7c0
Parker <ygfert@gmail.com>
2020-08-10 14:04:45 +0800

    Yarn add fontawesome

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// require("scripts")
+require("../scripts")
+// require("stylesheets")
+require("../stylesheets")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,5 +20,3 @@ require("channels")
 //
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
-
-console.log(456)
---
+++ app/frontend/stylesheets/index.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.css"
+import "./boards.scss";
+import "./form.scss";
+import "./grid.scss";
+import "./index.scss";
+import "./pages.scss";
+import "./posts.scss";
